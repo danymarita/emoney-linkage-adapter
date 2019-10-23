76ee1679c0fa9152f389d2c8980992973cba044d
Dany M Pradana <danympradana@gmail.com>
2019-10-23 11:14:52 +0700

    Commit ubah config

--- grpc-clients/linkage.js
+++ grpc-clients/linkage.js
@@ -3,12 +3,8 @@ const PROTO_PATH = "protos/linkage.proto";
 // var async = require('async');
 const grpc = require("grpc");
 const protoLoader = require("@grpc/proto-loader");
-// const config = require("config");
-require('dotenv').config();
 const grpc_server = process.env.LINKAGE_GRPC_SERVER;
 const basic_auth = process.env.LINKAGE_GRPC_BASIC_AUTH;
-// const grpc_server = config.get('grpc_server');
-// const basic_auth = config.get('grpc_server_basic_auth');
 
 const protoLoaderConf = protoLoader.loadSync(PROTO_PATH,
     {
